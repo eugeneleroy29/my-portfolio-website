21a19902c0d0f03b3d0a16ac168a2ce4b6c924bd
eugeneleroy29 <eugene.sunie@gmail.com>
2025-07-21 07:35:39 +0800

    added cv on pubic folder and updated about section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,7 +30,7 @@ const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href="#"
+                href="/eugene-cv.pdf"
                 download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
